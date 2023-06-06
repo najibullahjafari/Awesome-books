e09e21ca8dcaf0b316b9ec1bac6c9c9388bcfc36
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-06-06 04:47:47 +0430

    fix linter error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
-/* eslint-env node */
-
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // your rules here
+  },
+};
