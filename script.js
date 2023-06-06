44a362095a2c127e2ae2715b0d061bb12e85d96a
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-06-07 03:45:09 +0430

    Use classes

--- script.js
+++ script.js
@@ -1,62 +1,87 @@
+// CL
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class BookCollection {
+  constructor() {
+    this.books = this.loadBooksFromLocalStorage() || [];
+    this.render();
+  }
+
+  addBook(title, author) {
+    const book = new Book(title, author);
+    this.books.push(book);
+    this.saveBooksToLocalStorage();
+    this.render();
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.saveBooksToLocalStorage();
+    this.render();
+  }
+
+  saveBooksToLocalStorage() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  loadBooksFromLocalStorage() {
+    const storedBooks = localStorage.getItem('books');
+    return storedBooks ? JSON.parse(storedBooks).map(bookData => new Book(bookData.title, bookData.author)) : null;
+  }
+
+  render() {
+    const awesomeList = document.getElementById('awesomeList');
+    awesomeList.innerHTML = '';
+
+    this.books.forEach((book, index) => {
+      const bookItem = document.createElement('div');
+      bookItem.className = 'book-item';
+
+      const bookDetails = document.createElement('div');
+      bookDetails.className = 'book-details';
+      
+      const bookTextCon = document.createElement('div');
+      bookDetails.className = 'bookContainer';
+
+      const bookTitle = document.createElement('span');
+      bookTitle.className = 'book-title';
+      bookTitle.textContent = book.title;
+
+      const bookAuthor = document.createElement('span');
+      bookAuthor.className = 'book-author';
+      bookAuthor.textContent = book.author;
+
+      const removeBtn = document.createElement('button');
+      removeBtn.className = 'remove-btn';
+      removeBtn.textContent = 'Remove';
+      removeBtn.addEventListener('click', () => this.removeBook(index));
+
+      bookDetails.appendChild(bookTitle);
+      bookDetails.appendChild(document.createElement('br'));
+      bookDetails.appendChild(bookAuthor);
+
+      bookItem.appendChild(bookDetails);
+      bookItem.appendChild(removeBtn);
+
+      awesomeList.appendChild(bookItem);
+    });
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  const awesomeList = document.getElementById('awesomeList');
+  const bookForm = document.getElementById('bookForm');
   const textfieldTitle = document.getElementById('textfieldTitle');
   const textfieldAuthor = document.getElementById('textfieldAuthor');
-  const btnAdd = document.getElementById('btnAdd');
 
-  function createBookItem(title, author) {
-    const bookItem = document.createElement('div');
-    const bookTitle = document.createElement('span');
-    const br = document.createElement('br');
-    const bookAuthor = document.createElement('span');
-    const removeBtn = document.createElement('button');
-    const hr = document.createElement('hr');
-    const li = document.createElement('br');
-    const lii = document.createElement('br');
+  const bookCollection = new BookCollection();
 
-    bookTitle.textContent = title;
-    bookAuthor.textContent = author;
-    removeBtn.textContent = 'Remove';
-
-    removeBtn.addEventListener('click', function () {
-      bookItem.remove();
-      saveBooks();
-    });
-
-    bookItem.appendChild(bookTitle);
-    bookItem.appendChild(br);
-    bookItem.appendChild(bookAuthor);
-    bookItem.appendChild(li);
-    bookItem.appendChild(removeBtn);
-    bookItem.appendChild(hr);
-    bookItem.appendChild(lii);
-
-    awesomeList.appendChild(bookItem);
-  }
-
-  function saveBooks() {
-    const books = Array.from(awesomeList.children);
-
-    const bookData = [];
-
-    books.forEach(function (book) {
-      const title = book.querySelector('span').textContent;
-      const author = book.querySelector('span + br + span').textContent;
-      const bookObj = { title, author };
-      bookData.push(bookObj);
-    });
-    localStorage.setItem('books', JSON.stringify(bookData));
-  }
-
-  const storedBooks = localStorage.getItem('books');
-  if (storedBooks) {
-    const books = JSON.parse(storedBooks);
-    books.forEach(function (book) {
-      createBookItem(book.title, book.author);
-    });
-  }
-
-  btnAdd.addEventListener('click', function (event) {
+  bookForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const title = textfieldTitle.value;
@@ -66,9 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
       return;
     }
 
-    createBookItem(title, author);
-
-    saveBooks();
+    bookCollection.addBook(title, author);
 
     textfieldTitle.value = '';
     textfieldAuthor.value = '';
